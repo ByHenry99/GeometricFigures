01ea1d4e91815375ac39b9c0c70cfe0cbd593776
Usuario <Usuario@DESKTOP-BH5OFV8>
2026-09-05 09:05:47 -0500

    Add project files.

---
+++ GeometricFigures/GeometricFigure.cs
@@ -0,0 +1,21 @@
+﻿namespace Backend;
+
+public abstract class GeometricFigure
+{
+    // Constructors
+    protected GeometricFigure(string name)
+    {
+        Name = name;
+    }
+
+    //  Properties
+    public string Name { get; set; } = null!;
+
+    //  Public Methods
+    public abstract float GetArea();
+
+    public abstract float GetPerimenter();
+
+    public override string ToString() => $"{Name} + =>";
+
+}
