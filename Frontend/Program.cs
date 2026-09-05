0154d43b00b6a4bdbad80eaae81ff81eedafe947
Henry Esteban <zunigapalaciohenryesteban@gmail.com>
2026-09-05 12:26:51 -0500

    add Triangle

--- Frontend/Program.cs
+++ Frontend/Program.cs
@@ -6,12 +6,12 @@ var rhombus = new Rhombus(name: nameof(Rhombus), a: 5, d1: 7, d2: 10);
 var kite = new Kite(name: nameof(Kite), a: 7, b: 8, d1: 6, d2: 5);
 var rectangle = new Rectangle(name: nameof(Rectangle), a: 4.568f, b: 67.790f);
 var parallelogram = new Parallelogram(name: nameof(Parallelogram), a: 14.65f, b: 54.67f, h: 23.09f);
-/*var triangle = new Triangle(name: nameof(Triangle), a: 45.56, b: 12.34, c: 27.09, h: 15);
-var trapeze = new Trapeze(name: nameof(Trapeze), a: 10, b: 20, c: 30, d: 40, h: 20); */
+var triangle = new Triangle(name: nameof(Triangle), a: 45.56f, b: 12.34f, c: 27.09f, h: 15);
+/*var trapeze = new Trapeze(name: nameof(Trapeze), a: 10, b: 20, c: 30, d: 40, h: 20); */
 
 var figures = new List<GeometricFigure>
 {
-    circle, square, rhombus, kite, rectangle, parallelogram//, triangle, trapeze
+    circle, square, rhombus, kite, rectangle, parallelogram, triangle,// trapeze
 };
 
 foreach (var figure in figures)
---
+++ GeometricFigures/Triangle.cs
@@ -0,0 +1,48 @@
+﻿namespace Backend;
+
+public class Triangle : Rectangle
+{
+    // Fields
+    private float _c;
+    private float _h;
+
+    // Constructors
+    public Triangle(string name, float a, float b, float c, float h) : base(name, a, b)
+    {
+        C = c;
+        H = h;
+    }
+
+    // Properties
+    public float C 
+    { 
+        get => _c; 
+        set => _c = ValidateC(value); 
+    }
+
+    public float H 
+    { 
+        get => _h; 
+        set => _h = ValidateH(value); 
+    }
+
+    // Public Methods
+    public override float GetArea() => (B * H) / 2;
+
+    public override float GetPerimeter() => A + B + C;
+
+    // Private Methods
+    private float ValidateC(float c)
+    {
+        if (c <= 0)
+            throw new ArgumentException("The side length must be a positive number.");
+        return c;
+    }
+
+    private float ValidateH(float h)
+    {
+        if (h <= 0)
+            throw new ArgumentException("The height must be a positive number.");
+        return h;
+    }
+}
