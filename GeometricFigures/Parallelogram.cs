7a333575fc4bd477fdf19c80dc21088827a47d09
Henry Esteban <zunigapalaciohenryesteban@gmail.com>
2026-09-05 12:13:32 -0500

    add Parallelogram

---
+++ GeometricFigures/Parallelogram.cs
@@ -0,0 +1,33 @@
+﻿namespace Backend;
+
+public class Parallelogram : Rectangle
+{
+    // Fields
+    private float _h;
+
+    // Constructors
+    public Parallelogram(string name, float a, float b, float h) : base(name, a, b)
+    {
+        H = h;
+    }
+
+    // Properties
+    public float H
+    {
+        get => _h;
+        set => _h = ValidateH(value);
+    }
+
+    // Public Methods
+    public override float GetArea() => B * H;
+
+    public override float GetPerimeter() => 2 * (A + B);
+
+    // Private Methods
+    private float ValidateH(float h)
+    {
+        if (h <= 0)
+            throw new ArgumentException("Height must be a positive value.");
+        return h;
+    }
+}
